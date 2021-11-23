7a7e5f46d8b451502cd692e5d4693e98afbb69ae
Johannes Gabriel Brandau <johannes.g.brandau@gmail.com>
2021-11-23 15:43:22 +0100

    added Punch

--- KaomojiFighters/BattleScene.cs
+++ KaomojiFighters/BattleScene.cs
@@ -15,18 +15,12 @@ namespace KaomojiFighters
 {
     class Battle : Scene
     {
-        SpriteRenderer _player;
-        private SpriteRenderer renderer;
-        private int _horizontalMovementSpeed = 0;
-        private int _vertikalMovementSpeed = 0;
-        private FollowPlayer _enemy;
-
         public override void Initialize()
         {
             AddRenderer(new DefaultRenderer());
             base.Initialize();
-            CreateEntity("Kaomoji01").SetPosition(Screen.Center).AddComponent(new SpriteRenderer(Content.LoadTexture("Kaomoji01"))).AddComponent(new WASDMovement());
-            CreateEntity("Kaomoji02").SetPosition(1700,700).AddComponent(new SpriteRenderer(Content.LoadTexture("Kaomoji02"))).AddComponent(new FollowPlayer(){LerpIndex = 0.02f});
+            CreateEntity("Kaomoji01").SetPosition(Screen.Center).AddComponent(new Punch()).AddComponent(new WASDMovement());
+            CreateEntity("Kaomoji02").SetPosition(1700, 700).AddComponent(new SpriteRenderer(Content.LoadTexture("Kaomoji02"))).AddComponent(new FollowPlayer() { LerpIndex = 0.02f });
         }
     }
 }
---
+++ KaomojiFighters/Mobs/Punch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+using Nez;
+using Nez.Sprites;
+using Nez.Textures;
+using Rectangle = System.Drawing.Rectangle;
+
+namespace KaomojiFighters.Mobs
+{
+    class Punch : Component, IUpdatable
+    {
+        private int duration;
+        private Scene scene;
+        private BoxCollider collider;
+        private VirtualButton PunchButton;
+        SpriteRenderer EntitySprite;
+
+        public override void OnAddedToEntity()
+        {
+            scene = new Scene();
+            base.OnAddedToEntity();
+            EntitySprite = Entity.AddComponent(new SpriteRenderer(scene.Content.LoadTexture("Kaomoji01")));
+            collider = Entity.AddComponent(new BoxCollider(117, -50, 75, 75));
+            collider.Enabled = false;
+            PunchButton = new VirtualButton(new VirtualButton.MouseLeftButton());
+        }
+
+        public void Update()
+        {
+            if (PunchButton.IsPressed && duration == 0)
+            {
+                duration =25;
+                collider.Enabled  = true;
+            }
+            if (duration != 0)
+            {
+                EntitySprite.Sprite = new Sprite(scene.Content.LoadTexture("Kaomoji01Attack"));
+                duration--;
+                if (duration == 0)
+                {
+                    EntitySprite.Sprite = new Sprite(scene.Content.LoadTexture("Kaomoji01"));
+                    collider.Enabled = false;
+                }
+            }
+        }
+    }
+}
