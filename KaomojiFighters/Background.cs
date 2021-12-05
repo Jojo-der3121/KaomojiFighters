0cf5820cae44ac74c248c87bb0c5ef7dedec7341
Johannes Gabriel Brandau <johannes.g.brandau@gmail.com>
2021-12-05 15:24:58 +0100

    tried to implement a tiled Map

--- KaomojiFighters/Background.cs
+++ KaomojiFighters/Background.cs
@@ -13,12 +13,14 @@ namespace KaomojiFighters
     {
         public string BackgroundImageName;
         private SpriteRenderer _background;
+        private TiledMapRenderer Display;
         private Scene scene;
 
         public override void OnAddedToEntity()
         {
             scene = new Scene();
-            _background= Entity.AddComponent(new SpriteRenderer(scene.Content.LoadTexture(BackgroundImageName)));
+            _background = Entity.AddComponent(new SpriteRenderer(scene.Content.LoadTexture(BackgroundImageName)));
+            Display = Entity.AddComponent(new TiledMapRenderer(scene.Content.LoadTiledMap("KaomojiDisplayMap")));
             Entity.AddComponent(new AkatsukiEasterEgg());
             _background.RenderLayer = 1;
             _background.LayerDepth = 0.2f;
@@ -26,7 +28,7 @@ namespace KaomojiFighters
 
         public void Update()
         {
-            Entity.Position = new Vector2(Entity.Position.X +100*Time.DeltaTime, Entity.Position.Y);
+            Entity.Position = new Vector2(Entity.Position.X + 100 * Time.DeltaTime, Entity.Position.Y);
             if (Entity.Position.X >= 1920)
             {
                 Entity.Position = new Vector2(0, Entity.Position.Y);
