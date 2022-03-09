0ba5d10e74f5a3915e115b011fc69d58d917ba8a
Johannes Gabriel Brandau <johannes.g.brandau@gmail.com>
2022-03-09 15:34:47 +0100

    made the wasd movement static changed the Camera deadzone configurations

--- KaomojiFighters/Mobs/PlayerComponents/WASDMovement.cs
+++ KaomojiFighters/Mobs/PlayerComponents/WASDMovement.cs
@@ -10,15 +10,13 @@ namespace KaomojiFighters.Mobs
     class WASDMovement: Component, IUpdatable
     {
         private VirtualJoystick joystick;
-        private Vector2 LastFrameMovementSpeed;
 
         public override void OnAddedToEntity()=> joystick = new VirtualJoystick(true, new VirtualJoystick.KeyboardKeys(VirtualInput.OverlapBehavior.CancelOut, Keys.A, Keys.D, Keys.W, Keys.S));
 
 
         public void Update()
         {
-            LastFrameMovementSpeed = Vector2.Lerp(LastFrameMovementSpeed, joystick.Value * 13,  0.02f);
-            Entity.Position += LastFrameMovementSpeed;
+            Entity.Position += joystick.Value*10;
         }
     }
 }
--- KaomojiFighters/Scenes/OverworldScene.cs
+++ KaomojiFighters/Scenes/OverworldScene.cs
@@ -18,7 +18,7 @@ namespace KaomojiFighters.Scenes
             var player = CreateEntity("Player").AddComponent(new WASDMovement()).AddComponent(new Player());
             var webCame = FindEntity("camera");
                 webCame.AddComponent(new FollowCamera(player.Entity, FollowCamera.CameraStyle.CameraWindow)
-                {MapLockEnabled = false, FollowLerp = 0.5F, Camera = webCame.GetComponent<Camera>() });
+                {MapLockEnabled = false, FollowLerp = 0.5F, Camera = webCame.GetComponent<Camera>(), Deadzone = new RectangleF(800f,800f,490f,390f) });
             player.Transform.SetPosition(Screen.Center);
         }
     }
