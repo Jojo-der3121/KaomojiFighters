b5fbfe69890db3f24a251ae68668f0d7fbb10201
Johannes Gabriel Brandau <johannes.g.brandau@gmail.com>
2022-03-10 09:24:36 +0100

    addedoints to map

--- KaomojiFighters/Scenes/OverworldScene.cs
+++ KaomojiFighters/Scenes/OverworldScene.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nez.Tiled;
 
 namespace KaomojiFighters.Scenes
 {
@@ -16,6 +17,7 @@ namespace KaomojiFighters.Scenes
             var map = CreateEntity("Map").AddComponent(new TiledMapRenderer(Content.LoadTiledMap("OwOWorld"))) ;
             map.Transform.SetScale(10f);
             var player = CreateEntity("Player").AddComponent(new WASDMovement()).AddComponent(new Player());
+            
             var webCame = FindEntity("camera");
                 webCame.AddComponent(new FollowCamera(player.Entity, FollowCamera.CameraStyle.CameraWindow)
                 {MapLockEnabled = false, FollowLerp = 0.5F, Camera = webCame.GetComponent<Camera>(), Deadzone = new RectangleF(800f,800f,490f,390f) });
